39f52a3bc59dbe49d551125c310ac671d6326852
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-04-14 15:06:52 +0200

    few tweaks

--- scripts/collectd/collectd.py
+++ scripts/collectd/collectd.py
@@ -8,6 +8,7 @@ import fake_useragent as fu
 import bs4
 import queue as q
 import threading as t
+import csv
 
 
 class Randomizer():
@@ -63,7 +64,7 @@ URLS_PROC, PROD_PROC = t.Event(), t.Event()
 PROD_ID = 1
 
 def run_collector(brands):
-    procth = t.Thread(target=__proc_que)
+    procth = t.Thread(target=__proc_urls_que)
     procth.start()
     for b in brands:
         match b:
@@ -79,13 +80,11 @@ def run_collector(brands):
                 pass
 
 
-def __proc_que():
+def __proc_urls_que():
     cdir = os.path.dirname(__file__)
     while not URLS_PROC.is_set():
         with open(os.path.join(cdir, "resources/urls.txt"), "a") as f:
             for i in range(URLS.qsize()): f.write(URLS.get() + "\n")
-        sys.stdout.write(f"\r[COLLECTD] urls write")
-        sys.stdout.flush()
         time.sleep(20)
 
 
@@ -104,6 +103,11 @@ def __get_source_details(url):
 
 def __collect_urls(source):
     from random import randint
+    headers = {
+        "User-agent": rand.get_user_agent(),
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": rand.get_referer()
+    }
     for i in range(source.pages_k):
         res = requests.get(source.prod_url + "?page=" + str(i), headers=headers, proxies=rand.get_proxies())
         soup = bs4.BeautifulSoup(res.content, "html.parser")
@@ -111,49 +115,56 @@ def __collect_urls(source):
         for j in range(len(elems)):
             imgcs = elems[j].find_all(class_=re.compile("^ImageSection-module_imageSection"))
             for img in imgcs: URLS.put(source.base_url + img.find("a").get("href"))
-            sys.stdout.write(f"\r[COLLECTD] read {j + 1}/{len(elems)} urls")
+            sys.stdout.write(f"\r\033[K")
+            sys.stdout.write(f"\r(script) {j + 1}/{len(elems)} url")
             sys.stdout.flush()
-        sys.stdout.write(f"\r[COLLECTD] read {i + 1}/{source.pages_k} pages")
+        sys.stdout.write(f"\r\033[K")
+        sys.stdout.write(f"\r(script) {i + 1}/{source.pages_k} page")
         sys.stdout.flush()
         time.sleep(randint(5, 10))
     while URLS.qsize() != 0: continue;
     URLS_PROC.set()
-    sys.stdout.write(f"\r[COLLECTD] completed")
+    sys.stdout.write(f"\r(script) urls read completed")
     sys.stdout.flush()
 
 
-def run_processor():
+def run_proc():
+    procth = t.Thread(target=__proc_prod_que)
+    procth.start()
+    __proc_prod()
+
+
+def __proc_prod_que():
+    cdir = os.path.dirname(__file__)
+    while not PROD_PROC.is_set():
+        with open(os.path.join(cdir, "resources/products.csv"), "a", newline="") as f:
+            writer = csv.writer(f, delimiter=";")
+            for i in range(PRODUCTS.qsize()): writer.writerow(PRODUCTS.get())
+        time.sleep(10)
+
+
+def __proc_prod():
     from random import randint
     cdir = os.path.dirname(__file__)
     prod_k = __get_products_k()
     k, d = prod_k // 10, prod_k % 10
     for i in range(k + 1):
+        sys.stdout.write(f"\r\033[K")
+        sys.stdout.write(f"\r(script) {i + 1}/{k + 1} operation")
+        sys.stdout.flush()
         with open(os.path.join(cdir, "resources/urls.txt"), "r") as f:
             for j, ln in enumerate(f):
                 if i == k and j >= i * 10:
-                    __collect_prod()
+                    __collect_prod(ln)
                     time.sleep(randint(5, 10))
                 else:
                     if j < i * 10 or j >= (i * 10) + 10: continue
-                    __collect_prod()
+                    __collect_prod(ln)
                     time.sleep(randint(5, 10))
-        sys.stdout.write(f"\r[COLLECTD] proc {i + 1}/{k + 1} operations")
-        sys.stdout.flush()
-        time.sleep(1)
     while PRODUCTS.qsize() != 0: continue
-    PROC_PROD.set()
-    sys.stdout.write(f"\r[COLLECTD] completed")
+    PROD_PROC.set()
+    sys.stdout.write(f"\r(script) prod read completed")
     sys.stdout.flush()
-
-
-def _proc_prod_que():
-    cdir = os.path.dirname(__file__)
-    while not PROC_PROD.is_set():
-        with open(os.path.join(cdir, "resources/products.txt"), "a") as f:
-            for i in range(PRODUCTS.qsize()): f.write(PRODUCTS.get() + "\n")
-        sys.stdout.write(f"\r[COLLECTD] products write")
-        sys.stdout.flush()
-        time.sleep(10)
 
 
 def __get_products_k():
@@ -170,16 +181,11 @@ def __collect_prod(url):
     }
     res = requests.get(url, headers=headers, proxies=rand.get_proxies())
     soup = bs4.BeautifulSoup(res.content, "html.parser")
-    info = str(PROD_ID) + "; "
-    title = soup.find(class_=re.compile("^styles-module_titleBrand")).text + "; "
+    global PROD_ID
+    info = str(PROD_ID)
+    title = soup.find(class_=re.compile("^styles-module_titleBrand")).text
     elems = soup.find_all(class_=re.compile("^styles-module_productDescriptionContent"))
-    ean, desc = "", ""
-    for i in range(len(elems)):
-        if i < 2: desc += elems[i].text + "\n" + "; "
-        else: ean += elems[i].text[(elems[i].text.find("EAN") + 3):] + "; "
-    cat = url.split("/")[4] + "; "
-    info += ean + title + cat + desc
-    PRODUCTS.put(info)
+    PRODUCTS.put([PROD_ID, elems[2].text[(elems[2].text.find("EAN") + 3):], title, url.split("/")[4], elems[0].text, elems[1].text])
     PROD_ID += 1
 
 
@@ -193,7 +199,7 @@ if __name__ == "__main__":
     match len(sys.argv):
         case 1:
             try:
-                run_processor()
+                run_proc()
             except KeyboardInterrupt: pass
         case 2:
             if sys.argv[1] != "--collect-urls":
@@ -201,8 +207,8 @@ if __name__ == "__main__":
             else:
                 try:
                     brands = ["rossmann", "hebe"]
-                    # run_collector(brands)
-                    # run proc
+                    run_collector(brands)
+                    run_proc()
                 except KeyboardInterrupt: pass
         case _:
             print(f"script: usage: python3 collectd.py [--collect-urls]")
