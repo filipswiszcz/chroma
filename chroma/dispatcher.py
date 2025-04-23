ae79a9e9a3695931d1f182c63652237d3dc4cc47
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-04-23 17:22:36 +0200

    AtomicInteger, thread/process safe with compare-and-swap

--- chroma/dispatcher.py
+++ chroma/dispatcher.py
@@ -1,5 +1,8 @@
 from queue import Queue
-from threading import Thread
+from threading import Thread , Lock as TLock
+from multiprocessing import Lock as PLock
+from ctypes import c_int
+from typing import Union, ClassVar
 from helpers import Context, WORKERS
 
 # *************** Exception ***************
@@ -11,7 +14,34 @@ class DispatcherException(Exception):
 
 class Dispatcher:
     def __init__(self) -> None:
-        self.tasks = Queue()
-        self._workers = []
-    def start_workers(self) -> None:
-        pass
+        self._workers, self.tasks = [], Queue()
+        for _ in range(WORKERS):
+            self._workers.append(_Worker(daemon=True))
+
+# *************** Atomic ***************
+
+LockType = Union[TLock, PLock]
+
+class AtomicInteger:
+    def __init__(self, default=0, multiprocessing=False) -> None:
+        self._value, self.__lock = c_int(default), TLock() if not multiprocessing else PLock()
+    def get_and_increment(self) -> int:
+        while True:
+            current = self._value
+            if self.__compare_and_set(current.value, current.value + 1): return current.value
+    def __compare_and_set(self, expected, updated) -> bool:
+        with self.__lock:
+            if self._value.value == expected:
+                self._value.value = updated; return True
+            return False
+    def __enter__(self): return self
+    def __exit__(self) -> None:
+        if hasattr(self.__lock, "close"): self.__lock.close()
+        return None
+
+# *************** Worker ***************
+
+class _Worker(Thread):
+    WORKER_ID: ClassVar[AtomicInteger] = AtomicInteger()
+    def __init__(self) -> None:
+        super().__init__(name=f"Worker-{_Worker.WORKER_ID.get_and_increment()}", daemon=True)
