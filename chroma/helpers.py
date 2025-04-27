c65cb4d6edec489b09c98a94197b5bb47fb833b8
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-04-27 14:45:12 +0200

    add __future__ annotations, ContextVar value accepts only int

--- chroma/helpers.py
+++ chroma/helpers.py
@@ -1,8 +1,9 @@
-import os
-import contextlib
+from __future__ import annotations
+import os, functools, contextlib
 from typing import Any, ClassVar, Dict
 
 
+@functools.cache
 def getenv(key, default=0): return type(default) (os.getenv(key, default))
 
 # *************** Context ***************
@@ -12,35 +13,24 @@ class Context(contextlib.ContextDecorator):
         self.kwargs = kwargs
     def __enter__(self) -> None:
         self.old_context: dict = {k: v.value for k, v in ContextVar._cache.items()}
-        for k, v in self.kwargs.items():
-            ContextVar._cache[k].value = v
+        for k, v in self.kwargs.items(): ContextVar._cache[k].value = v
     def __exit__(self, *args) -> None:
-        for k, v in self.old_context.items():
-            ContextVar._cache[k].value = v
+        for k, v in self.old_context.items(): ContextVar._cache[k].value = v
 
 class ContextVar:
-    _cache: ClassVar[dict[str, "ContextVar"]] = {}
-    def __init__(self, key, default_value) -> None:
-        if key in ContextVar._cache:
-            raise RuntimeError(f"attempt to recreate ContextVar {key}")
+    _cache: ClassVar[dict[str, ContextVar]] = {}
+    key: str
+    value: int
+    def __init__(self, key, default) -> None:
+        if key in ContextVar._cache: raise RuntimeError(f"Attempt to recreate ContextVar {key}")
         ContextVar._cache[key] = self
-        self.key, self.value = key, default_value
-    def set(self, value) -> None:
-        self.value = value
-    def get(self) -> Any:
-        return self.value
-    def __bool__(self) -> bool:
-        return bool(self.value)
-    def __ge__(self, other) -> bool:
-        return self.value >= other
-    def __gt__(self, other) -> bool:
-        return self.value > other
-    def __le__(self, other) -> bool:
-        return self.value <= other
-    def __lt__(self, other) -> bool:
-        return self.value < other
+        self.key, self.value = key, getenv(key, default)
+    def __bool__(self) -> bool: return bool(self.value)
+    def __ge__(self, other) -> bool: return self.value >= other
+    def __gt__(self, other) -> bool: return self.value > other
+    def __le__(self, other) -> bool: return self.value <= other
+    def __lt__(self, other) -> bool: return self.value < other
 
-DEBUG, WORKERS = ContextVar("DEBUG", 0), ContextVar("WORKERS", 5)
-HEADERS = ContextVar("HEADERS", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
+DEBUG, INSTANCES, WORKERS = ContextVar("DEBUG", 0), ContextVar("INSTANCES", 1), ContextVar("WORKERS", 5)
 
 # *************** Cache ***************
