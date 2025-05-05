386585dab8e76fa7f65e520ae53360061949d1de
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-05-05 23:49:51 +0200

    command abstr, __builtin_commands() imlp

--- chroma/cli.py
+++ chroma/cli.py
@@ -1,13 +1,35 @@
+from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Dict, List, Callable
 from helpers import DEBUG
+
+# *************** Command ***************
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self, args: List[str]) -> None: pass
+
+class _HelpCommand(Command):
+    def __init__(self, command: Callable = None) -> None:
+        self.command = command
+    def execute(self, args: List[str]) -> None:
+        self.command(args)
+
+class _ExitCommand(Command):
+    def __init__(self, command: Callable = None) -> None:
+        self.command = command
+    def execute(self, args: List[str]) -> None:
+        self.command(args)
 
 # *************** Console ***************
 
 class CLI:
     def __init__(self) -> None:
-        self._commands: Dict[str, Callable] = {"help": self.__help_command, "exit": self.__exit_command}
+        self._commands: Dict[str, Command] = {}
+        self.__builtin_commands()
         self.shutdown = False
+    def __builtin_commands(self) -> None:
+        self._commands["help"], self._commands["exit"] = _HelpCommand(self.__help_command), _ExitCommand(self.__exit_command)
     def start(self) -> None:
         while not self.shutdown:
             try:
@@ -23,7 +45,7 @@ class CLI:
         self._commands[name] = command
     def execute_command(self, name: str, args: List[str]) -> None:
         if name in self._commands:
-            try: self._commands[name](args)
+            try: self._commands[name].execute(args)
             except Exception as exc:
                 if DEBUG > 0: print(f"Error occurred while executing command. {exc}")
         else: print(f"Unknown command. Type 'help' for available commands")
