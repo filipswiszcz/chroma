086132bcde2d737434374de0fbdd5a8d261bfcac
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-05-25 23:26:53 +0200

    http connection pooling

--- chroma/request/request.py
+++ chroma/request/request.py
@@ -1,6 +1,19 @@
 from urllib.request import Request, ProxyHandler, build_opener
 from headers import Randomizer
 
+# *************** Pool ***************
+
+class _HTTPConnectionPool(__ConnectionPool):
+    def __init__(self, host: str, port: int = None, timeout: int = 5, maxsize: int = 1,
+        headers: str = None, retries: int = None) -> None:
+            pass
+
+# _HTTPSConnectionPool
+
+class __ConnectionPool:
+    def __init__(self, host: str, port: int = None) -> None:
+        pass
+
 
 RANDOMIZER = Randomizer()
 
@@ -8,3 +21,5 @@ def get(url: str) -> "response obj":
     handler = ProxyHandler(RANDOMIZER.get_proxies())
     opener = build_opener(handler)
     request = Request(url, headers=RANDOMIZER.get_headers()) # get_headers returns str
+    try: response = opener.open(request)
+    except: pass
